4c8475b868f392ceb110dc029ab2e0dbb0be042c
Yang Guo <yangguoif@gmail.com>
2024-11-05 13:37:22 +1300

    wip

--- frontend/package/build/index.ts
+++ frontend/package/build/index.ts
@@ -1,9 +1,22 @@
 import moment from "moment";
+import * as fs from "fs";
+import * as path from "path";
 
 function test() {
-  const date = moment().format("LL");
+  const date = moment().format('MMMM Do YYYY, h:mm:ss a');
 
   console.log(date);
+  const filePath = path.join(__dirname, "test.json");
+  const content = {
+      date: date,
+  };
+  const json = JSON.stringify(content, null, 4);
+
+  fs.writeFile(filePath, json, "utf8", (error) => {
+      if (error) throw error;
+      console.log(`updated system icons`);
+  });
+
 }
 
 test();
